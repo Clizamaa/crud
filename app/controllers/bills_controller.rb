83a4f9494fc8fab4aadaf667b4012c586a51bbed
Christian <cfla86@gmail.com>
2022-01-31 07:41:37 -0300

    crud hecho

---
+++ app/controllers/bills_controller.rb
@@ -0,0 +1,47 @@
+class BillsController < ApplicationController
+  
+  def index
+    @bills = Bill.all
+  end
+
+  def show
+     @bill = Bill.find(params[:id])
+  end
+
+  def new
+    @bill = Bill.new
+  end
+
+  def create
+    @bill = Bill.new(bill_params)
+    if @bill.save
+      redirect_to @bill
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @bill = Bill.find(params[:id])
+  end
+
+  def update
+    @bill = Bill.find(params[:id])
+    if @bill.update(bill_params)
+      redirect_to @bill
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @bill = Bill.find(params[:id])
+    @bill.destroy
+    redirect_to root_path 
+  end
+  
+  private
+  def bill_params
+    params.require(:bill).permit(:name, :price)
+  end
+end
